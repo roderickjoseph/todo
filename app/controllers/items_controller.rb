e5b63189ef8b2856e8b7a90fc69cfefa3d7476dc
roderickjoseph <rjgauvin@gmail.com>
2017-03-27 02:04:14 +0000

    Completed destroy functionality and little style tweaks to navbar

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,13 @@ class ItemsController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to root_path
+  end
+
+
   private
 
   def item_params
