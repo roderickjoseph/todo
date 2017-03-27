849c53658317f65ceeb0dd22852a914c2cfe71f4
roderickjoseph <rjgauvin@gmail.com>
2017-03-27 01:31:03 +0000

    Added update functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,14 +9,27 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(place_params)
+    Item.create(item_params)
     redirect_to root_path
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update_attributes(item_params)
+    redirect_to root_path
+  end
 
   private
 
-  def place_params
+  def item_params
     params.require(:item).permit(:name, :description, :date)
   end
 end
